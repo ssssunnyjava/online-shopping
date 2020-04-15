60fd129b10507c6fc2947705cfe38c93f4f20737
ssssunnyjava <ssssunnyjava@gmail.com>
2020-04-15 16:50:15 +0530

    My Project setup have completed successfully.

---
+++ shoppingbackend/src/main/java/com/sunny/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.sunny.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
